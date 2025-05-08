96f72001a014fed03c8cbcfda60c17cdb3364c15
Anusha <anushard2681998@gmail.com>
2025-05-08 18:25:00 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./public/*.html'],
+    theme: {
+      extend: {
+        // You can add customizations here, for example, custom colors, fonts, etc.
+      },
+    },
+    plugins: [],
+  }
+  
