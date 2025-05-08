9442c0c3c7b352457d1d80d9e0d93f277814760c
Anusha <anushard2681998@gmail.com>
2025-05-08 19:31:24 +0530

    changed public to docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./public/*.html'],
+    content: ['./docs/*.html'],
     theme: {
       extend: {
         // You can add customizations here, for example, custom colors, fonts, etc.
